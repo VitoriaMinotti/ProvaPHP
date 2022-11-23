b4bbc6010a5a4f88a0ebe78ec334f5d0449ea2e1
VitoriaMinotti <vitorialminotti@hotmail.com>
2022-11-23 08:16:06 -0300

    mudanca de img

--- index.php
+++ index.php
@@ -1,3 +1,3 @@
 <?php include ('header.php')?>
 
-<img src="img/tempo-do-quiz-banner-de-luz-de-elemento-de-design-de-sinal-de-neon_77399-1401.webp" class="d-block w-100" style="height: 561px;" alt="banner-quiz" />
+<img src="img/banner.jpg" class="d-block w-100"  alt="banner-quiz" />
