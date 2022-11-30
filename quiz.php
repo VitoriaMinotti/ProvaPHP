04d0082666668e5082318c372c29bd646e47a445
VitoriaMinotti <vitorialminotti@hotmail.com>
2022-11-29 21:09:17 -0300

    correcao da pagina resposta

--- quiz.php
+++ quiz.php
@@ -7,9 +7,9 @@ $resultado = mysqli_query($conexao, $query);
 ?>  
 
 <body style="background-color: #D8BFD8;">
-  <div class="col-5 mt-5 mx-auto bg-light border rounded container-fluid">
+  <div class="col-6 mt-5 mx-auto bg-light border rounded container-fluid">
     
-    <div class="mx-auto mt-3" style="width: 60%">
+    <div class="mx-auto mt-3" style="width: 20%">
       <h3 >Responda</h3>
     </div>
 
@@ -33,59 +33,18 @@ $resultado = mysqli_query($conexao, $query);
       ?>
       <?php
     }
-
-      ?><br>     
+      ?><br>
       <?php
-    }
+    }?>
     
-    echo "<button type='submit'>Enviar</button>";
-    echo "<button><a href='.' style='text-decoration:none; color: black'>Voltar</a></button>";
+    <div class="d-flex">
+      <button type="submit" class="ms-3 btn btn-success">Salvar</button>
+      <button type="button" class="ms-3 btn btn-warning"><a href="index.php" style="text-decoration:none; color: white">Voltar</a></button>
+    </div>
+    <?php
     echo "</form>";
   ?>
 
-</div>
-
-        <!-- <form class="mx-auto mt-3" style="width: 40%" action="alternativas.php?pergunta_id=<?php echo $_GET["pergunta_id"]; ?>" method="post" >
-          <input type="hidden" name="pergunta_id" value="<?php echo $_GET["pergunta_id"]; ?>"/>
-
-          <div class="d-flex">
-            <input class="form-control border rounded container-fluid" type="text" name="1"/>
-            <input type="radio" name="correto" value="1">
-          </div>
-          <br>
-
-          <div class="d-flex">
-            <input class="form-control border rounded container-fluid" type="text" name="1"/>
-            <input type="radio" name="correto" value="2">
-            <br>
-          </div>
-          <br>
-
-          <div class="d-flex">
-            <input class="form-control border rounded container-fluid" type="text" name="1"/>
-            <input type="radio" name="correto" value="3">
-          </div>
-          <br>
-
-          <div class="d-flex">
-            <input class="form-control border rounded container-fluid" type="text" name="1"/>
-            <input type="radio" name="correto" value="4">
-          </div>
-          <br>
-
-          <div class="d-flex">
-            <input class="form-control border rounded container-fluid" type="text" name="1"/>
-            <input type="radio" name="correto" value="5">
-          </div>
-          <br>
-
-          <div class="d-flex">
-            <button type="submit" class="btn btn-success">Salvar</button>
-            <button type="button" class="btn btn-warning"><a href="." style="text-decoration:none; color: white">Voltar</a></button>
-          </div>
-          <br>
-        </form> -->
-      </div>
 </div>
 </body>
 
--- resposta.php
+++ resposta.php
@@ -2,43 +2,52 @@
 include "conexao.php";
 include "header.php";
 
-
 $query = "select * from perguntas";
 $resultado = mysqli_query($conexao, $query);
 ?>  
 
-<div style="padding: 10px;" class="col-9 mt-5 bg-light border rounded container-fluid">
-<h3 >Respostas</h3>
-<br>
-<form action="resposta.php" method="post" >
-  <?php while($linha = mysqli_fetch_array($resultado))
-    {
-      echo "<td>".$linha['id']."- </td>";
-      echo "<td>".$linha['pergunta']."</td><br>";
+<body style="background-color: #D8BFD8;">
+  <div class="col-6 mt-5 mx-auto bg-light border rounded container-fluid">
+    
+    <div class="mx-auto mt-3" style="width: 20%">
+      <h3 >Respostas</h3>
+    </div>
+
+    <form class="p-3 m-4 p-5 mx-auto" action="resposta.php" method="post" >
+      <?php while($linha = mysqli_fetch_array($resultado))
+        {
+          echo "<td>".$linha['id']."- </td>";
+          echo "<td>".$linha['pergunta']."</td><br>";
       ?> 
     <?php
-     
       $queryAlternativas = "select * from alternativas where pergunta_id=".$linha['id']."";
       $resultadoAlternativa = mysqli_query($conexao, $queryAlternativas);
 
       $queryAltCorreta = "select * from alternativas where pergunta_id=".$linha['id']." and correta = 1";
       $resultadoAltCorreta = mysqli_query($conexao, $queryAltCorreta);
 
-      while($resposta = mysqli_fetch_array($resultadoAlternativa))
+     while($resposta = mysqli_fetch_array($resultadoAlternativa))
       {
              echo "<p>" .$resposta['alternativa']."</p>";
         while($correto = mysqli_fetch_array($resultadoAltCorreta))
         {
-          if ( $_POST['correto'.$linha['id']] == $correto['alternativa'] ) { 
-            echo "<p style = 'background-color: green'> " .$resposta['alternativa']. "</p>";
+          if ( $_POST['correto'.$linha['id']] != $correto['alternativa'] ) { 
+            echo "<p style = 'background-color: red'> " .$resposta['alternativa']. "</p>";
           }
           else{
-            echo "<p style = 'background-color: red;margin-bottom:0 '> " .$resposta['alternativa']. "</p>";
+            echo "<p style = 'background-color: green;margin-bottom:0 '> " .$correto['alternativa']. "</p>";
           }
-        }
-    }
-  }
-    echo "<button><a href='.' style='text-decoration:none; color: black'>Voltar</a></button>";
+        }}
+      ?><br>
+      <?php
+        }?>
+    
+    <div class="d-flex">
+      <button type="submit" class="ms-3 btn btn-success">Salvar</button>
+      <button type="button" class="ms-3 btn btn-warning"><a href="quiz.php" style="text-decoration:none; color: white">Voltar</a></button>
+    </div>
+    <?php
     echo "</form>";
   ?>
 </div>
+</body>
